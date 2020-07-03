5dafbd27477e0063c17cde994787d64920a93abf
Luca Baffa <lb803@mailbox.org>
2020-07-03 19:42:06 +0100

    add accessibility summary

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
-from PyQt5.Qt import QWidget, QHBoxLayout, QCheckBox, QGroupBox
+from PyQt5.Qt import QWidget, QHBoxLayout, QCheckBox, QGroupBox, QLabel, QLineEdit
 from calibre.utils.config import JSONConfig
 
 prefs = JSONConfig('plugins/access_aide')
 
 # Set defaults
 prefs.defaults['force_override'] = False
-
+prefs.defaults['access'] = {
+    'accessibilitySummary': 'This publication conforms to WCAG 2.0 AA.'
+    #'accessMode': ['textual', 'visual'],
+    #'accessModeSufficient': ['textual', 'visual'],
+    #'accessibilityFeature': 'structuralNavigation'
+    }
 
 class ConfigWidget(QWidget):
 
@@ -25,5 +30,22 @@ class ConfigWidget(QWidget):
         options_layout.addWidget(self.force_override)
         self.force_override.setChecked(prefs['force_override'])
 
+        # Accessibility options
+        access = QGroupBox(_('Accessibility'), self)
+        self.l.addWidget(access)
+        access_layout = QHBoxLayout()
+        access.setLayout(access_layout)
+
+        self.label = QLabel('Accessibility Summary:')
+        access_layout.addWidget(self.label)
+
+        self.msg = QLineEdit(self)
+        self.msg.setText(prefs['access']['accessibilitySummary'])
+        access_layout.addWidget(self.msg)
+        self.label.setBuddy(self.msg)
+
     def save_settings(self):
         prefs['force_override'] = self.force_override.isChecked()
+        prefs['access'] = {
+            'accessibilitySummary': self.msg.text()
+            }
